c4cece2957a5c2ea6adf56599cd3853faaa4dd6d
Stanislaw09 <sgardzielewski09@gmail.com>
2020-08-11 21:54:52 +0200

    favourites option & sharing to social media

--- src/quotes.js
+++ src/quotes.js
@@ -1,10 +1,33 @@
-import React,{useEffect, useState} from 'react'
-import {Typography, makeStyles, Button, Card, Avatar, Collapse, Link, IconButton, Menu, MenuItem} from '@material-ui/core'
+import React,{useState} from 'react'
+import {Typography,
+    makeStyles,
+    Card,
+    Avatar,
+    Collapse,
+    Link,
+    IconButton,
+    Menu,
+    MenuItem} from '@material-ui/core'
 import ShareIcon from '@material-ui/icons/Share'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import DeleteIcon from '@material-ui/icons/Delete'
 import FavoriteIcon from '@material-ui/icons/Favorite'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import MenuIcon from '@material-ui/icons/Menu'
+
+
+import {FacebookShareButton,
+    FacebookIcon,
+    FacebookMessengerShareButton,
+    FacebookMessengerIcon,
+    TwitterShareButton,
+    TwitterIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+    RedditShareButton,
+    RedditIcon,
+    EmailShareButton,
+    EmailIcon} from 'react-share'
 const firebase=require('firebase')
 
 const useStyles=makeStyles(theme=>({
@@ -61,24 +84,33 @@ const useStyles=makeStyles(theme=>({
         height: '50px',
         transform: 'rotate(180deg)',
         transition: theme.transitions.create('transform', {
-          duration: theme.transitions.duration.shortest,
+            duration: theme.transitions.duration.shortest,
         })
-      },
-      expanded:{
+    },
+    expanded:{
         padding: '10px',
         margin: '2px',
         height: '50px',
         transform: 'rotate(0deg)',
         transition: theme.transitions.create('transform', {
-          duration: theme.transitions.duration.shortest,
+            duration: theme.transitions.duration.shortest,
         })
-      },
+    },
     icons:{
         width: '30px',
         height: '30px'
     },
     cardContent:{
         display: 'flex'
+    },
+    shareContainers:{
+        padding: '10px'
+    },
+    share:{
+        width: '50px',
+        height: '50px',
+        margin: '5px',
+        borderRadius: '50%'
     }
 }))
 
@@ -86,13 +118,26 @@ const CardView=props=>{
     const classes=useStyles()
     const [showText, setShowText]=useState(false)
     const [menuAnchor, setMenuAnchor]=useState(null)
+    const [shareMenu, setShareMenu]=useState(false)
 
     const handleDelete=event=>{
         firebase.firestore().collection('quotes').doc(props.quote.id).delete().then(()=>{
             console.log("deleted")
         }).catch(error=>{
-            console.error("Shit happens: ",error)
+            console.error("Shit happens:",error)
         })
+        setMenuAnchor(null)
+    }
+
+    const handleFavourite=()=>{
+        firebase.firestore().collection('quotes').doc(props.quote.id).set({
+            favourite: !props.quote.favourite,
+            url: props.quote.url,
+            date: props.quote.date,
+            favIcon: props.quote.favIcon,
+            text: props.quote.text
+        })
+
         setMenuAnchor(null)
     }
 
@@ -102,9 +147,11 @@ const CardView=props=>{
                 <div className={classes.subHeader}>
                     <Avatar src={props.quote.favIcon} className={classes.avatar}/>
 
-                    <Typography><Link href={props.quote.url} target='_blank' className={classes.link}>
-                        {props.quote.url.replace('http://','').replace('https://','').split(/[/?#]/)[0]}
-                    </Link></Typography>
+                    <Typography>
+                        <Link href={props.quote.url} target='_blank' className={classes.link}>
+                            {props.quote.url.replace('http://','').replace('https://','').split(/[/?#]/)[0]}
+                        </Link>
+                    </Typography>
 
                     <Typography className={classes.date}>{props.quote.date}</Typography>
                 </div>
@@ -119,14 +166,80 @@ const CardView=props=>{
                     anchorEl={menuAnchor}
                     onClose={()=>setMenuAnchor(false)}
                     className={classes.menu}>
-                    <MenuItem onClick={()=>setMenuAnchor(null)}><FavoriteIcon className={classes.menuItem}/>Favourite</MenuItem>
-                    <MenuItem onClick={()=>setMenuAnchor(null)}><ShareIcon className={classes.menuItem}/>Share</MenuItem>
-                    <MenuItem onClick={handleDelete}><DeleteIcon className={classes.menuItem}/>Delete</MenuItem>
+
+                    <MenuItem onClick={handleFavourite}>
+                        {
+                            props.quote.favourite ?
+                                <FavoriteIcon className={classes.menuItem}/> :
+                                <FavoriteBorderIcon className={classes.menuItem}/>
+                        }Favourite
+                    </MenuItem>
+
+                    <MenuItem onClick={handleDelete}>
+                        <DeleteIcon className={classes.menuItem}/>Delete
+                    </MenuItem>
+
+                    <MenuItem onClick={()=>setShareMenu(prev=>!prev)}>
+                        <ShareIcon className={classes.menuItem}/>Share
+                    </MenuItem>
+
+                    <Collapse in={shareMenu}>
+                        <div className={classes.shareContainers}>
+                            <FacebookShareButton
+                                url={props.quote.url}
+                                quote={props.quote.text}
+                            hashtag="#quote">
+                                <FacebookIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </FacebookShareButton>
+
+                            <FacebookMessengerShareButton url={props.quote.url}>
+                                <FacebookMessengerIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </FacebookMessengerShareButton>
+
+                            <TwitterShareButton url={props.quote.url}>
+                                <TwitterIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </TwitterShareButton>
+                        </div>
+
+                        <div className={classes.shareContainers}>
+                            <WhatsappShareButton
+                                url={props.quote.url}>
+                                <WhatsappIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </WhatsappShareButton>
+
+                            <RedditShareButton url={props.quote.url}>
+                                <RedditIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </RedditShareButton>
+
+                            <EmailShareButton url={props.quote.url}>
+                                <EmailIcon
+                                    logoFillColor="white"
+                                    className={classes.share}
+                                    onClick={()=>setMenuAnchor(null)}/>
+                            </EmailShareButton>
+                        </div>
+                    </Collapse>
+
                 </Menu>
             </div>
 
             <div className={classes.cardContent}>
-                {props.quote.text.length>200 &&
+                {props.quote.text.length>160 &&
                     <IconButton
                         onClick={()=>setShowText(prev=>!prev)}
                         className={showText ? classes.expand : classes.expanded}>
@@ -142,13 +255,12 @@ const CardView=props=>{
     )
 }
 
-
 export const Quotes=props=>{
     const classes=useStyles()
 
     return(
         <div className={classes.container}>
-            {props.quotes && props.quotes.map(quote=><CardView quote={quote}/>)}
+            {props.quotes && props.quotes.map((quote,i)=><CardView key={i} quote={quote}/>)}
         </div>
     )
 }
